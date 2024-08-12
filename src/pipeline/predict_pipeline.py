0ddda044a2c5d251f6c13d2928fff4a89953a6c0
nileshhhhhh <2020s18045@stu.cmb.ac.lk>
2024-08-12 15:20:25 +0530

    changed predit pipeline

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -2,6 +2,8 @@ import sys
 import pandas as pd
 from src.exception import CustomException
 from src.utils import load_object
+import os
+
 
 class PredictPipeline:
     def __init__(self):
@@ -9,23 +11,21 @@ class PredictPipeline:
 
     def predict(self,features):
         try:
-            model_path="artifacts\model.pkl"
-            preprocessor_path='artifacts\preprocessor.pkl'
-            print("Before Loading")
+            model_path=os.path.join("artifacts","model.pkl")
+            preprocessor_path=os.path.join('artifacts','preprocessor.pkl')
             model=load_object(file_path=model_path)
             preprocessor=load_object(file_path=preprocessor_path)
-            print("After Loading")
             data_scaled=preprocessor.transform(features)
             preds=model.predict(data_scaled)
             return preds
         
         except Exception as e:
             raise CustomException(e,sys)
-    
-        
+
+
 
 class CustomData:
-    def __init__(self,
+    def __init__(  self,
         gender: str,
         race_ethnicity: str,
         parental_level_of_education,
@@ -47,7 +47,7 @@ class CustomData:
         self.reading_score = reading_score
 
         self.writing_score = writing_score
-    
+
     def get_data_as_data_frame(self):
         try:
             custom_data_input_dict = {
@@ -64,4 +64,3 @@ class CustomData:
 
         except Exception as e:
             raise CustomException(e, sys)
-                  
